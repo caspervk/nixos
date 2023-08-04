ba079f03e2a5149c89deb9f35b22f5e43f2e6a5a
Casper V. Kristensen <casper@vkristensen.dk>
2023-08-04 13:13:06 +0200

    some vim config

--- modules/base/vim.nix
+++ modules/base/vim.nix
@@ -1,8 +1,30 @@
-{ ... }: {
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
+{ home-manager, pkgs, ... }: {
+  home-manager.users.caspervk = {
+    programs.neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+      plugins = with pkgs.vimPlugins; [
+        vim-sleuth
+      ];
+      extraPackages = with pkgs; [ ];
+      extraConfig = ''
+        set number
+        set relativenumber
+
+        set scrolloff=3
+
+        set ignorecase
+        set smartcase
+
+        set tabstop=4
+        set softtabstop=4
+        set shiftwidth=4
+        set expandtab
+      '';
+    };
   };
+  environment.variables.EDITOR = "vim";
 }
