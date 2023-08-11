8d75216c3e876cd9ce9adfba877ef05d876fa016
Casper V. Kristensen <EMAIL>
2023-08-11 19:03:18 +0200

    static ip

--- hosts/tor/default.nix
+++ hosts/tor/default.nix
@@ -6,16 +6,18 @@
     ../../modules/tor
   ];
 
-  networking = {
-    hostName = "tor";
-    interfaces.ens3.ipv6.addresses = [{
-      address = "2a0d:3e83:0001:b284::1";
-      prefixLength = 64;
-    }];
-    defaultGateway6 = {
-      # https://www.ssdvps.dk/knowledgebase/18/IPv6-Gateway.html
-      address = "2a0d:3e83:1::1";
-      interface = "ens3";
+  networking.hostName = "tor";
+  systemd.network.networks = {
+    "10-lan" = {
+      name = "ens3";
+      address = [
+        "91.210.59.57/25"
+        "2a0d:3e83:1:b284::1/64"
+      ];
+      routes = [
+        { routeConfig = { Gateway = "91.210.59.1"; }; }
+        { routeConfig = { Gateway = "2a0d:3e83:1::1"; GatewayOnLink = true; }; }
+      ];
     };
   };
 
