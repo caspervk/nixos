5b95274cacf7c88efdc2d5458edca7125bc35d08
Casper V. Kristensen <casper@vkristensen.dk>
2023-08-12 16:07:10 +0200

    Syncthing: add omega

--- modules/desktop/syncthing.nix
+++ modules/desktop/syncthing.nix
@@ -10,17 +10,18 @@
         id = "WES3JH4-S34HTC5-42YZHUJ-MX3Z6PA-PFO72KA-YIJMDOB-GQWZXZ3-I7BBTAS";
         addresses = [ "tcp://lambda.caspervk.net" ];
       };
+      "omega" = { id = "EZIQ7SI-Y6BBLUY-QI4EEYU-UNIXPSG-R6X5E77-AA2UC7S-VRV2LKQ-RNBOGQT"; };
       "S10e" = { id = "DWC6YHB-FRYKFHD-FPOUITV-7GL2WZH-RSFOJXR-PHYXDO7-74NLBUZ-TZENVAC"; };
       "zeta" = { id = "GQRNHAQ-MMRQYMD-P4RCA6I-5DJ3HXO-J2N2GVP-UGI55YR-HD3EYSO-ERU5QQV"; };
     };
     folders = {
       "keepass" = {
         path = "~/keepass";
-        devices = [ "lambda" "S10e" "zeta" ];
+        devices = [ "lambda" "omega" "S10e" "zeta" ];
       };
       "sync" = {
         path = "~/sync";
-        devices = [ "lambda" "zeta" ];
+        devices = [ "lambda" "omega" "zeta" ];
       };
     };
   };
