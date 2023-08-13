843f73340966443e7da4471e0a847ec0f5ea82b7
Casper V. Kristensen <casper@vkristensen.dk>
2023-08-13 19:36:49 +0200

    remove fake unicode

--- modules/desktop/network.nix
+++ modules/desktop/network.nix
@@ -8,7 +8,7 @@
 
   # systemd-networkd-wait-online can timeout and fail if there are no
   # network interfaces available for it to manage. When systemd-networkd is
-  # enabled but a different service is responsible for managing the system’s
+  # enabled but a different service is responsible for managing the system's
   # internet connection (for example, NetworkManager), this service is unnecessary and can be disabled.
   # https://search.nixos.org/options?channel=23.05&show=systemd.network.wait-online.enable
   systemd.network.wait-online.enable = false;
