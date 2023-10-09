eb6ddeb7202370096213a725c474b8b73fa754d1
Casper V. Kristensen <casper@vkristensen.dk>
2023-10-09 13:46:35 +0200

    mu: display configuration

--- hosts/mu/sway.nix
+++ hosts/mu/sway.nix
@@ -8,7 +8,18 @@
             mode = "1920x1080@60.052Hz";
             position = "0,0";
           };
+          "AOC Q27T1G5 0x000007C8" = {
+            mode = "2560x1440@74.968Hz";
+            position = "1920,200";
+          };
+          "AOC Q27T1G5 0x0000080B" = {
+            mode = "2560x1440@74.968Hz";
+            position = "4480,200";
+          };
         };
+        workspaceOutputAssign = [
+          { workspace = "8"; output = "eDP-1"; }
+        ];
       };
     };
   };
