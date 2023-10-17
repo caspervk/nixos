21dd10134ef01909eeb3396b80caf488e940fa49
Casper V. Kristensen <vk@magenta.dk>
2023-10-17 17:11:07 +0200

    disable jet engine

--- hosts/mu/hardware.nix
+++ hosts/mu/hardware.nix
@@ -38,4 +38,21 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # Disable jet engine
+  services.thinkfan = {
+    enable = true;
+    levels = [
+      [ 0 0 70 ]
+      [ 1 65 75 ]
+      [ 2 70 80 ]
+      [ 3 75 85 ]
+      [ 6 80 90 ]
+      [ 7 90 95 ]
+      [ "level auto" 95 32767 ]
+    ];
+  };
+  boot.extraModprobeConfig = ''
+    options thinkpad_acpi fan_control=1 experimental=1
+  '';
 }
