57da8cf8a2e197f67b71e41721f9115e1e733881
Casper V. Kristensen <casper@vkristensen.dk>
2023-11-09 01:18:06 +0100

    Revert "tor: explicit cpu configuration"
    This reverts commit cf32b1e4a39573ba3b93a88bdcc5526301ef015d.

--- hosts/tor/tor.nix
+++ hosts/tor/tor.nix
@@ -2,8 +2,6 @@
   services.tor = {
     settings = {
       Nickname = "DXV7520";
-      # Auto-detection of CPUs does not seem to work
-      NumCPUs = 2;
       ORPort = [
         { addr = "91.210.59.57"; port = 443; }
         { addr = "[2a0d:3e83:1:b284::1]"; port = 443; }
