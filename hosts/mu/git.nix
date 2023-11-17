cfbf954472674e098c728507d97e86a1d3b128b3
Casper V. Kristensen <vk@magenta.dk>
2023-11-17 13:23:03 +0100

    git mr

--- hosts/mu/git.nix
+++ hosts/mu/git.nix
@@ -2,6 +2,11 @@
   home-manager.users.caspervk = {
     programs.git = {
       userEmail = lib.mkForce "vk@magenta.dk";
+      aliases = {
+        # https://docs.gitlab.com/ee/user/project/push_options.html
+        mr = "push --push-option=merge_request.create --push-option=merge_request.assign='vk'";
+      };
+
     };
   };
 }
