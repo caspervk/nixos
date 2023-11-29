584bdaf10fcddc85393344f1556b4d207b63ceb6
Casper V. Kristensen <casper@vkristensen.dk>
2023-11-29 01:20:43 +0100

    replace gitlab-ci with dumb Containerfile

--- modules/server/system.nix
+++ modules/server/system.nix
@@ -2,7 +2,7 @@
   # Automatically `nixos-rebuild switch` daily with the latest configuration
   # from git. This overwrites any uncommitted changes in ~/nixos/, which is why
   # it is only enabled on servers. Note that this requires updating flake.lock
-  # in the repository periodically (see .gitlab-ci.yml). Alternatively, at the
+  # in the repository periodically (see Containerfile). Alternatively, at the
   # cost of reproducability, add
   # flags = [ "--recreate-lock-file" "--no-write-lock-file" ]
   # to ignore the repository flake.lock and use the latest input versions.
