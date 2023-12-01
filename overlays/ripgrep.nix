206be0599b5012368d2828d1a5542d2acedae9b9
Casper V. Kristensen <casper@vkristensen.dk>
2023-12-01 20:57:14 +0100

    23.11: no more need for ripgrep overlay

--- overlays/ripgrep.nix
+++
@@ -1,7 +0,0 @@
-{ home-manager, home-manager-unstable, ... }: {
-  # ripgrep isn't in Home Manager v23.05
-
-  home-manager.users.caspervk = {
-    imports = [ "${home-manager-unstable}/modules/programs/ripgrep.nix" ];
-  };
-}
