0fb83615bcd73f0ac02d618dfd84e77f6cbc2e7e
Casper V. Kristensen <vk@magenta.dk>
2023-12-06 18:58:07 +0100

    nvim: indent-blankline v3

--- modules/base/vim/config.lua
+++ modules/base/vim/config.lua
@@ -11,9 +11,7 @@ require('kanagawa').setup({
 })
 
 -- https://github.com/lukas-reineke/indent-blankline.nvim
-require('indent_blankline').setup({
-    show_current_context = true,
-})
+require('ibl').setup()
 
 -- https://github.com/numToStr/Comment.nvim
 require("Comment").setup()
