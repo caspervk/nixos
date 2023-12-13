c68b04978294ea5f78c884aa5de3a322eb02618b
Casper V. Kristensen <casper@vkristensen.dk>
2023-12-13 20:25:40 +0100

    Revert "23.11: remove workaround for network-online.target"
    This reverts commit 6a24fbfacf4a7dbc6666f59e2804e537442e2ea5.

--- modules/desktop/network.nix
+++ modules/desktop/network.nix
@@ -17,4 +17,12 @@
       { directory = "/etc/NetworkManager/system-connections"; user = "root"; group = "root"; mode = "0700"; }
     ];
   };
+
+  # systemd-networkd-wait-online can timeout and fail if there are no network
+  # interfaces available for it to manage. When systemd-networkd is enabled but
+  # a different service is responsible for managing the system's internet
+  # connection (for example, NetworkManager), this service is unnecessary and
+  # can be disabled.
+  # https://search.nixos.org/options?channel=23.11&show=systemd.network.wait-online.enable
+  systemd.network.wait-online.enable = false;
 }
