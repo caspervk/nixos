10d326644b88b846f812f9d7bda035ce8a453953
Casper V. Kristensen <vk@magenta.dk>
2024-01-29 14:45:35 +0100

    terraform => tofu

--- hosts/mu/fish.nix
+++ hosts/mu/fish.nix
@@ -2,6 +2,7 @@
   home-manager.users.caspervk = {
     programs.fish.shellAliases = {
       sm = "bw get totp e2be31fb-135f-4b28-88cd-b094000ddb67 | wl-copy; gcloud --project magenta-os2mo-production compute ssh --tunnel-through-iap saltmaster";
+      terraform = "tofu";
     };
   };
 }
