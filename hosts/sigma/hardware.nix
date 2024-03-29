e33a9538fb8ee5f186a927d4ad017449b41c1e73
Casper V. Kristensen <casper@vkristensen.dk>
2024-03-29 20:34:57 +0100

    sigma: nvidia 💩

--- hosts/sigma/hardware.nix
+++ hosts/sigma/hardware.nix
@@ -10,7 +10,7 @@
     (modulesPath + "/installer/scan/not-detected.nix")
     nixos-hardware.nixosModules.common-cpu-amd
     nixos-hardware.nixosModules.common-cpu-amd-pstate
-    #nixos-hardware.nixosModules.common-gpu-amd
+    nixos-hardware.nixosModules.common-gpu-nvidia
     nixos-hardware.nixosModules.common-pc
     nixos-hardware.nixosModules.common-pc-ssd
   ];
