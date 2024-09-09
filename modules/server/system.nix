650935d170955fb63c8ec50d0daecb961d0aebf8
Casper V. Kristensen <casper@vkristensen.dk>
2024-09-09 22:37:44 +0200

    fix autoupgrade comment

--- modules/server/system.nix
+++ modules/server/system.nix
@@ -6,10 +6,7 @@
   # Automatically `nixos-rebuild switch` daily with the latest configuration
   # from git. This overwrites any uncommitted changes in ~/nixos/, which is why
   # it is only enabled on servers. Note that this requires updating flake.lock
-  # in the repository periodically (see Containerfile). Alternatively, at the
-  # cost of reproducability, add
-  # flags = [ "--recreate-lock-file" "--no-write-lock-file" ]
-  # to ignore the repository flake.lock and use the latest input versions.
+  # in the repository periodically (see .gitea/workflows/update.yaml).
   system.autoUpgrade = {
     enable = true;
     flake = "git+https://git.caspervk.net/caspervk/nixos.git";
