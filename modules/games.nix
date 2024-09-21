6756d30e2d1c1d1985c7a646408e62c92a96e200
Casper V. Kristensen <casper@vkristensen.dk>
2024-09-21 02:44:49 +0200

    wine 🍷

--- modules/games.nix
+++ modules/games.nix
@@ -1,10 +1,16 @@
 {pkgs, ...}: {
   # https://wiki.nixos.org/wiki/Lutris
   # https://wiki.nixos.org/wiki/Steam
+  # https://wiki.nixos.org/wiki/Wine
+  #
+  # After install, run
+  # > winetricks corefonts d3dx9 d3dx10 dxvk
+  # to set up .wine/.
+
   programs.steam.enable = true;
 
   environment.systemPackages = with pkgs; [
-    steam-run
+    dxvk
     (lutris.override {
       extraLibraries = pkgs: [
         # List library dependencies here
@@ -13,5 +19,8 @@
         # List package dependencies here
       ];
     })
+    wineWowPackages.fonts
+    wineWowPackages.waylandFull
+    winetricks
   ];
 }
