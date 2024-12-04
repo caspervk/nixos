37ca945396dc3a967397f6bdf2df95be2203307c
Casper V. Kristensen <casper@vkristensen.dk>
2024-12-04 10:12:51 +0100

    omega: disable adaptive sync
    Causes the screen to flicker slightly.

--- hosts/omega/sway.nix
+++ hosts/omega/sway.nix
@@ -7,7 +7,6 @@
           "ASUSTek COMPUTER INC ROG XG27AQ M3LMQS370969" = {
             mode = "2560x1440@144.006Hz";
             position = "0,0";
-            adaptive_sync = "on";
           };
           "BNQ BenQ XL2411Z SCD06385SL0" = {
             mode = "1920x1080@144.001Hz";
