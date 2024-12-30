a05cd2471a21718957cf8ccf53d1936ffefa48dd
Casper V. Kristensen <casper@vkristensen.dk>
2024-12-30 02:31:06 +0100

    vim: go lsp

--- modules/base/vim.nix
+++ modules/base/vim.nix
@@ -517,6 +517,11 @@
 
               local lspconfig = require("lspconfig")
 
+              -- https://github.com/golang/tools/tree/master/gopls
+              lspconfig.gopls.setup({
+                capabilities = capabilities,
+              })
+
               -- https://github.com/nix-community/nixd
               lspconfig.nixd.setup({
                 capabilities = capabilities,
@@ -908,6 +913,7 @@
         nixpkgs-unstable.legacyPackages.${pkgs.system}.basedpyright # lsp
         nixpkgs-unstable.legacyPackages.${pkgs.system}.ruff # lsp/conform
         pkgs.alejandra # conform
+        pkgs.gopls # lsp
         pkgs.nixd # lsp
         pkgs.nodePackages.prettier # conform
         pkgs.opentofu # conform
