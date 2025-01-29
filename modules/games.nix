4c41fd689f4b48d6ace69c4b2d4fb03e3f9fb461
Casper V. Kristensen <casper@vkristensen.dk>
2025-01-29 02:41:38 +0100

    games: remove lutris, dxvk

--- modules/games.nix
+++ modules/games.nix
@@ -1,26 +1,11 @@
 {pkgs, ...}: {
-  # https://wiki.nixos.org/wiki/Lutris
   # https://wiki.nixos.org/wiki/Steam
-  # https://wiki.nixos.org/wiki/Wine
-  #
-  # After install, run
-  # > winetricks corefonts d3dx9 d3dx10 dxvk
-  # to set up .wine/.
-
   programs.steam.enable = true;
 
   environment.systemPackages = with pkgs; [
-    dxvk
-    (lutris.override {
-      extraLibraries = pkgs: [
-        # List library dependencies here
-      ];
-      extraPkgs = pkgs: [
-        # List package dependencies here
-      ];
-    })
-    wineWowPackages.fonts
+    # https://wiki.nixos.org/wiki/Wine
     wineWowPackages.waylandFull
+    wineWowPackages.fonts
     winetricks
   ];
 }
