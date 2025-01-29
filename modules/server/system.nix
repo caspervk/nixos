da5bc333be7f39626a7b4f2a8c242623516fd2d2
Casper V. Kristensen <casper@vkristensen.dk>
2025-01-29 01:26:41 +0100

    autoupgrade: retry on failure

--- modules/server/system.nix
+++ modules/server/system.nix
@@ -10,10 +10,20 @@
   system.autoUpgrade = {
     enable = true;
     flake = "git+https://git.caspervk.net/caspervk/nixos.git";
+    randomizedDelaySec = "45min";
   };
 
-  # The `nixos-secrets` flake input requires authentication
-  systemd.services.nixos-upgrade.environment.GIT_SSH_COMMAND = "ssh -i ${config.age.secrets.autoupgrade-deploy-key.path}";
+  systemd.services.nixos-upgrade = {
+    # Retry on failure, but stop if the service fails three times in five
+    # minutes. Useful to properly upgrade services which fail due to
+    # intermittent network issues.
+    serviceConfig.Restart = "on-failure";
+    serviceConfig.RestartSec = "30s";
+    unitConfig.StartLimitIntervalSec = "5min";
+    unitConfig.StartLimitBurst = 3;
+    # The `nixos-secrets` flake input requires authentication
+    environment.GIT_SSH_COMMAND = "ssh -i ${config.age.secrets.autoupgrade-deploy-key.path}";
+  };
 
   age.secrets.autoupgrade-deploy-key = {
     file = "${secrets}/secrets/autoupgrade-deploy-key.age";
