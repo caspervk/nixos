f516a6bc492ef900fb99b5cb51f0026b78a6f6b6
Casper V. Kristensen <casper@vkristensen.dk>
2025-03-15 17:48:00 +0100

    games: proton-ge

--- modules/games.nix
+++ modules/games.nix
@@ -1,6 +1,18 @@
-{pkgs, ...}: {
+{
+  nixpkgs-unstable,
+  pkgs,
+  ...
+}: {
   # https://wiki.nixos.org/wiki/Steam
-  programs.steam.enable = true;
+  programs.steam = {
+    enable = true;
+    extraCompatPackages = [
+      # Custom Proton. 'Force the use of a specific Steam Play compatibility
+      # tool' in each game's properties in Steam.
+      # https://github.com/GloriousEggroll/proton-ge-custom
+      nixpkgs-unstable.legacyPackages.${pkgs.system}.proton-ge-bin
+    ];
+  };
 
   environment.systemPackages = with pkgs; [
     # https://wiki.nixos.org/wiki/Wine
