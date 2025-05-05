04e52fa58a03a4865c2403f2de7ed212cb06bc03
Casper V. Kristensen <casper@vkristensen.dk>
2025-05-06 00:16:35 +0200

    desktop: how does lutris and proton work?

--- modules/games.nix
+++ modules/games.nix
@@ -16,6 +16,8 @@
 
   environment.systemPackages = with pkgs; [
     # https://wiki.nixos.org/wiki/Lutris
+    # Lutris uses the system's wine or proton from steam (or downloaded
+    # through ProtonPlus/ProtonUp). Check steam if proton is missing.
     lutris
     # https://wiki.nixos.org/wiki/Wine
     wineWowPackages.waylandFull
