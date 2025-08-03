685a5e9638cae35243333b159fce8b952fa5f8a8
Casper V. Kristensen <casper@vkristensen.dk>
2025-08-03 05:35:41 +0200

    git: colour blame by age

--- modules/base/git.nix
+++ modules/base/git.nix
@@ -19,6 +19,38 @@
       };
 
       extraConfig = {
+        blame.coloring = "highlightRecent";
+        color.blame.highlightRecent = builtins.concatStringsSep "," [
+          # 28-step OKLAB gradient
+          "#37474f"
+          "23 month ago, #3d4d51"
+          "22 month ago, #445354"
+          "21 month ago, #4b5856"
+          "20 month ago, #515e58"
+          "19 month ago, #58645a"
+          "18 month ago, #5f6a5c"
+          "17 month ago, #66705e"
+          "16 month ago, #6d7660"
+          "15 month ago, #747c62"
+          "14 month ago, #7b8364"
+          "13 month ago, #838965"
+          "12 month ago, #8a8f67"
+          "11 month ago, #919569"
+          "10 month ago, #999c6a"
+          "9 month ago, #a0a26b"
+          "8 month ago, #a8a86d"
+          "7 month ago, #b0af6e"
+          "6 month ago, #b7b56f"
+          "5 month ago, #bfbc70"
+          "4 month ago, #c7c271"
+          "3 month ago, #cfc972"
+          "2 month ago, #d7cf73"
+          "1 month ago, #dfd674"
+          "3 week ago, #e7dd74"
+          "2 week ago, #efe375"
+          "1 week ago, #f7ea76"
+          "1 day ago, #fff176"
+        ];
         diff.algorithm = "histogram";
         diff.colorMoved = "default";
         init.defaultBranch = "master";
