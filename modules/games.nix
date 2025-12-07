0077d277fa3178ce28df15d1de12e6c7b1498888
Casper V. Kristensen <casper@vkristensen.dk>
2025-12-07 23:26:40 +0100

    games: mangohud

--- modules/games.nix
+++ modules/games.nix
@@ -9,6 +9,9 @@
     # Lutris uses the system's wine or proton from steam (or downloaded
     # through ProtonPlus/ProtonUp). Check steam if proton is missing.
     lutris
+    # Vulkan and OpenGL overlay for monitoring FPS, temperatures, CPU/GPU load
+    # and more.
+    mangohud
     # https://wiki.nixos.org/wiki/Wine
     winetricks
     wineWowPackages.fonts
