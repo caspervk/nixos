f3aac836e4e9c3f83493140b0009ea79be8f3b6b
Casper V. Kristensen <casper@vkristensen.dk>
2026-01-20 23:15:42 +0100

    flake: don't pass `system` to `nixpkgs.lib.nixosSystem`
    It's set in `hardware.nix`.

--- flake.nix
+++ flake.nix
@@ -61,43 +61,36 @@
     nixosConfigurations = {
       # Hetzner VPS
       alpha = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
         specialArgs = inputs; # pass flake inputs to modules
         modules = [./hosts/alpha];
       };
       # Hetzner VPS
       delta = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
         specialArgs = inputs; # pass flake inputs to modules
         modules = [./hosts/delta];
       };
       # Work laptop
       mu = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
         specialArgs = inputs; # pass flake inputs to modules
         modules = [./hosts/mu];
       };
       # Home desktop
       omega = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
         specialArgs = inputs; # pass flake inputs to modules
         modules = [./hosts/omega];
       };
       # Home Server
       sigma = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
         specialArgs = inputs; # pass flake inputs to modules
         modules = [./hosts/sigma];
       };
       # Tor relay
       tor = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
         specialArgs = inputs; # pass flake inputs to modules
         modules = [./hosts/tor];
       };
       # Laptop
       zeta = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
         specialArgs = inputs; # pass flake inputs to modules
         modules = [./hosts/zeta];
       };
