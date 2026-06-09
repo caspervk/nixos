0280592cefa9a6031468be94bc4e9d88c0d7456f
Casper V. Kristensen <casper@vkristensen.dk>
2026-06-10 00:55:18 +0200

    git: update config

--- modules/base/delta.nix
+++ modules/base/delta.nix
@@ -9,6 +9,7 @@
       enableJujutsuIntegration = true;
       options = {
         line-numbers = true;
+        navigate = true;
         side-by-side = true;
       };
     };
--- modules/base/git.nix
+++ modules/base/git.nix
@@ -7,49 +7,112 @@
       enable = true;
       settings = {
         alias = {
-          # https://docs.gitlab.com/ee/user/project/push_options.html
+          # Push and create GitLab merge request
+          # https://docs.gitlab.com/topics/git/commit/#push-options
           mr = "push --push-option=merge_request.create --push-option=merge_request.assign='vk'";
+          # Safer force push: refuse to overwrite the remote if it has commits
+          # we haven't seen or integrated.
+          pushf = "push --force-with-lease --force-if-includes";
         };
-        blame.coloring = "highlightRecent";
-        branch.autoSetupMerge = "simple";
+        blame = {
+          # Highlight more recent changes like in JetBrains IDEs
+          coloring = "highlightRecent";
+        };
+        branch = {
+          # Only set up upstream tracking for a new branch when it branches off
+          # a remote-tracking branch of the same name.
+          autoSetupMerge = "simple";
+          # Sort branches by most recently committed to
+          sort = "-committerdate";
+        };
+        checkout = {
+          # Assume origin when a branch exists on multiple remotes
+          defaultRemote = "origin";
+        };
         color.blame.highlightRecent = builtins.concatStringsSep "," [
           # 28-step OKLAB gradient
           "#37474f"
-          "23 month ago, #3d4d51"
-          "22 month ago, #445354"
-          "21 month ago, #4b5856"
-          "20 month ago, #515e58"
-          "19 month ago, #58645a"
-          "18 month ago, #5f6a5c"
-          "17 month ago, #66705e"
-          "16 month ago, #6d7660"
-          "15 month ago, #747c62"
-          "14 month ago, #7b8364"
-          "13 month ago, #838965"
-          "12 month ago, #8a8f67"
-          "11 month ago, #919569"
-          "10 month ago, #999c6a"
-          "9 month ago, #a0a26b"
-          "8 month ago, #a8a86d"
-          "7 month ago, #b0af6e"
-          "6 month ago, #b7b56f"
-          "5 month ago, #bfbc70"
-          "4 month ago, #c7c271"
-          "3 month ago, #cfc972"
-          "2 month ago, #d7cf73"
-          "1 month ago, #dfd674"
-          "3 week ago, #e7dd74"
-          "2 week ago, #efe375"
-          "1 week ago, #f7ea76"
+          "23 months ago, #3d4d51"
+          "22 months ago, #445354"
+          "21 months ago, #4b5856"
+          "20 months ago, #515e58"
+          "19 months ago, #58645a"
+          "18 months ago, #5f6a5c"
+          "17 months ago, #66705e"
+          "16 months ago, #6d7660"
+          "15 months ago, #747c62"
+          "14 months ago, #7b8364"
+          "13 months ago, #838965"
+          "12 months ago, #8a8f67"
+          "11 months ago, #919569"
+          "10 months ago, #999c6a"
+          "9 months ago, #a0a26b"
+          "8 months ago, #a8a86d"
+          "7 months ago, #b0af6e"
+          "6 months ago, #b7b56f"
+          "5 months ago, #bfbc70"
+          "4 months ago, #c7c271"
+          "3 months ago, #cfc972"
+          "2 months ago, #d7cf73"
+          "1 months ago, #dfd674"
+          "3 weeks ago, #e7dd74"
+          "2 weeks ago, #efe375"
+          "1 weeks ago, #f7ea76"
           "1 day ago, #fff176"
         ];
-        diff.algorithm = "histogram";
-        diff.colorMoved = "default";
-        init.defaultBranch = "master";
-        pull.rebase = true;
-        push.autoSetupRemote = true;
-        rebase.autoSquash = true;
-        rebase.autoStash = true;
+        commit = {
+          # Show the whole commit diff in the commit message editor
+          verbose = true;
+        };
+        diff = {
+          # Make diffs more readable
+          algorithm = "histogram";
+          # Use different colours to highlight moved lines
+          colorMoved = "default";
+        };
+        fetch = {
+          # Automatically delete remote-tracking branches that no longer exist
+          # on the remote.
+          prune = true;
+        };
+        init = {
+          defaultBranch = "master"; # TODO
+        };
+        merge = {
+          # Make conflicts more readable
+          conflictStyle = "zdiff3";
+        };
+        pull = {
+          # Automatically add --rebase
+          rebase = true;
+        };
+        push = {
+          # Push new branches without having to --set-upstream
+          autoSetupRemote = true;
+          # When pushing commits, also push relevant annotated tags
+          followTags = true;
+        };
+        rebase = {
+          # Automatically reorder and apply fixup and squash commits during
+          # interactive rebase.
+          autoSquash = true;
+          # Automatically stash uncommitted changes before rebasing, and apply
+          # them afterwards.
+          autoStash = true;
+          # Automatically update other branches that point to the commits being
+          # rebased. Makes stacked branches much easier to handle.
+          updateRefs = true;
+        };
+        rerere = {
+          # Remember how we resolved conflicts so Git can automatically resolve
+          # it the next time it sees the same one.
+          enabled = true;
+          autoupdate = true;
+        };
+        tag = {
+          # Sort tags by semantic version, rather than lexicographically
+          sort = "-version:refname";
+        };
         user = {
           name = "Casper V. Kristensen";
           email = "casper@vkristensen.dk";
