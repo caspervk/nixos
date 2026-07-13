442d5f089aa934cf9cd6671748287dc9e9067989
Casper V. Kristensen <casper@vkristensen.dk>
2026-07-13 03:19:37 +0200

    knot-resolver: upgrade to v6

--- hosts/delta/knot-resolver.nix
+++ hosts/delta/knot-resolver.nix
@@ -3,143 +3,112 @@
   pkgs,
   ...
 }: {
-  # Knot Resolver is a minimalistic implementation of a caching validating DNS
-  # resolver. Modular architecture keeps the core tiny and efficient, and it
-  # provides a state-machine like API for extensions.
-  # https://knot-resolver.readthedocs.io/en/stable/index.html
+  # Knot Resolver is an open-source implementation of a caching validating DNS
+  # resolver.
+  # https://www.knot-resolver.cz/documentation/latest/
   #
   # Test resolver:
-  # > nix shell nixpkgs#knot-dns
-  # > kdig -d @dns.caspervk.net example.com
-  # > kdig -d +https @dns.caspervk.net example.com
-  # > kdig -d +tls @dns.caspervk.net example.com
   #
-  # Connect to control socket:
-  # > nix shell nixpkgs#socat -c sudo socat readline UNIX-CONNECT:/run/knot-resolver/control/1
-  # >> help()
-  # >> cache.clear("example.com")
-  # https://knot-resolver.readthedocs.io/en/stable/daemon-scripting.html
-  services.kresd = {
+  #   nix shell nixpkgs#knot-dns
+  #   kdig -d @dns.caspervk.net example.com
+  #   kdig -d +https @dns.caspervk.net example.com
+  #   kdig -d +tls @dns.caspervk.net example.com
+  #   kdig -d +quic @dns.caspervk.net example.com
+  #
+  # Clear cache:
+  #
+  #   sudo kresctl cache clear example.com.
+  #
+  services.knot-resolver = {
     enable = true;
-    # Extra features required for the lua cqueue library, which is required to
-    # watch for changes in rpz blocklist.
-    # TODO: upgrade do knot-resolver v6
-    package = pkgs.knot-resolver_5.override {extraFeatures = true;};
-    # For maximum performance there should be as many kresd processes as there
-    # are available CPU threads.
-    # https://knot-resolver.readthedocs.io/en/stable/systemd-multiinst.html
-    instances = 2;
-    extraConfig =
-      # lua
-      ''
-        -- Explicitly only listen on external addresses to allow
-        -- systemd-resolved to use localhost:53 as on every other system.
-        local addresses = {
-          -- Blocks spam and advertising domains
-          ipv4_filtered = "159.69.4.2",
-          ipv6_filtered = "2a01:4f8:1c0c:70d1::1",
-          -- Don't censor anything. This is primarily for the tor exit relay,
-          -- since not censoring anything is kind of the whole point of tor.
-          ipv6_unfiltered = "2a01:4f8:1c0c:70d1::2",
+    settings = {
+      network = {
+        listen = [
+          # Blocks spam and advertising domains
+          {
+            interface = ["159.69.4.2" "2a01:4f8:1c0c:70d1::1"];
+            kind = "dns";
+          }
+          {
+            interface = ["159.69.4.2" "2a01:4f8:1c0c:70d1::1"];
+            kind = "doh2";
+          }
+          {
+            interface = ["159.69.4.2" "2a01:4f8:1c0c:70d1::1"];
+            kind = "dot";
+          }
+          {
+            interface = ["159.69.4.2" "2a01:4f8:1c0c:70d1::1"];
+            kind = "doq";
+          }
+          # Doesn't censor anything. This is primarily for the tor exit relay,
+          # since not censoring anything is kind of the whole point of tor.
+          {
+            interface = ["2a01:4f8:1c0c:70d1::2"];
+            kind = "dns";
+          }
+          {
+            interface = ["2a01:4f8:1c0c:70d1::2"];
+            kind = "doh2";
+          }
+          {
+            interface = ["2a01:4f8:1c0c:70d1::2"];
+            kind = "dot";
+          }
+          {
+            interface = ["2a01:4f8:1c0c:70d1::2"];
+            kind = "doq";
+          }
+        ];
+        tls = {
+          cert-file = "${config.security.acme.certs."caspervk.net".directory}/fullchain.pem";
+          key-file = "${config.security.acme.certs."caspervk.net".directory}/key.pem";
+        };
+      };
+      cache = {
+        # The documentation recommends allocating 90% of the machine's free
+        # memory for the resolver cache. The server has 4 GiB ram.
+        size-max = "2048M";
+        prefetch = {
+          # Any time the resolver answers with records that are about to
+          # expire, they get refreshed. Record is expiring if it has less than
+          # 1% TTL (or less than 5s). That improves latency for records which
+          # get frequently queried, relatively to their TTL.
+          expiring = true;
+        };
+      };
+      # TODO: https://www.knot-resolver.cz/documentation/latest/config-rate-limiting.html
+      views = [
+        {
+          subnets = ["0.0.0.0/0"];
+          dst-subnet = "159.69.4.2";
+          tags = ["blocklist"];
         }
-        for _, addr in pairs(addresses) do
-          net.listen(addr, 53, {kind = "dns"})
-          net.listen(addr, 853, {kind = "tls"})
-          net.listen(addr, 443, {kind = "doh2"})
-        end
-
-        -- TLS certificate for DoT and DoH
-        -- https://knot-resolver.readthedocs.io/en/stable/daemon-bindings-net_tlssrv.html
-        net.tls(
-          "${config.security.acme.certs."caspervk.net".directory}/fullchain.pem",
-          "${config.security.acme.certs."caspervk.net".directory}/key.pem"
-        )
-
-        -- Beware that cache is shared by *all* requests. It is safe to refuse
-        -- (policy.DENY) answer based on who asks the resolver, but trying to
-        -- serve different data to different clients (policy.ANSWER) will result
-        -- in unexpected behavior.
-        -- https://knot-resolver.readthedocs.io/en/stable/modules-view.html
-        -- https://knot-resolver.readthedocs.io/en/stable/modules-policy.html#response-policy-zones
-        -- `true` means watch file for changes
-        local blocklist = policy.rpz(policy.DENY, "/tmp/rpz-pro.txt", true)
-        modules.load("view")
-        -- `true` means apply view based on query destination rather than source
-        view:addr(addresses.ipv4_filtered, blocklist, true)
-        view:addr(addresses.ipv6_filtered, blocklist, true)
-
-        -- Cache is stored in /var/cache/knot-resolver, which is persisted to
-        -- disk. Choosing a cache size that can fit into RAM is important even
-        -- if the cache is stored on disk. Otherwise, the extra I/O caused by
-        -- disk access for missing pages can cause performance issues.
-        -- The server has 4 GB ram.
-        -- >> cache.stats()["usage_percent"]
-        -- https://knot-resolver.readthedocs.io/en/stable/daemon-bindings-cache.html
-        cache.size = 2048 * MB
-
-        -- The predict module helps to keep the cache hot by prefetching records.
-        -- It can utilize two independent mechanisms to select the records which
-        -- should be refreshed: expiring records and prediction.
-        -- The expiring records mechanism is always active and is not
-        -- configurable. Any time the resolver answers with records that are about
-        -- to expire, they get refreshed.
-        -- https://knot-resolver.readthedocs.io/en/stable/modules-predict.html
-        modules.load("predict")
-        -- The prediction mechanism is prototype and not recommended for use in
-        -- production. It is disabled by by configuring period=0.
-        predict.config({period = 0})
-
-        -- Test domain to verify DNS server is being used
-        policy.add(
-          policy.domains(
-            policy.ANSWER({ [kres.type.A] = {rdata = kres.str2ip("192.0.2.0"), ttl = 5} }),
-            policy.todnames({"test.dns.caspervk.net"})
-          )
-        )
-      '';
-  };
-
-  # Ensure an empty blocklist exists so knot-resolver can start. We need
-  # working DNS to fetch the blocklist, so we must start knot-resolver first.
-  systemd.tmpfiles.rules = [
-    "f /tmp/rpz-pro.txt 0644 knot-resolver knot-resolver"
-  ];
-
-  # Update the blocklist after when knot-resolver starts, and then once per day
-  systemd.services.kresd-blocklist-updater = {
-    after = ["network.target" "kresd@.service"];
-    wants = ["kresd@.service"];
-    # requiredBy = ["kresd@.service"];
-    # before = ["kresd@.service"];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.wget}/bin/wget --timestamping --output-document=/tmp/rpz-pro.txt https://raw.githubusercontent.com/hagezi/dns-blocklists/refs/heads/main/rpz/pro.txt";
-      User = "knot-resolver";
-      Group = "knot-resolver";
-    };
-  };
-  systemd.timers.kresd-blocklist-updater = {
-    wantedBy = ["timers.target"];
-    timerConfig = {
-      OnBootSec = "5m";
-      OnUnitActiveSec = "24h";
-      Unit = "kresd-blocklist-updater.service";
+        {
+          subnets = ["::/0"];
+          dst-subnet = "2a01:4f8:1c0c:70d1::1";
+          tags = ["blocklist"];
+        }
+      ];
+      local-data = {
+        rpz = [
+          {
+            file = pkgs.runCommand "stevenblack-blocklist-rpz" {} ''
+              ${pkgs.hosts-bl}/bin/Hosts-BL -i ${pkgs.stevenblack-blocklist}/hosts -f rpz -o $out
+            '';
+            tags = ["blocklist"];
+          }
+        ];
+        addresses = {
+          # Test domain to verify DNS server is being used
+          "test.dns.caspervk.net" = "192.0.2.0";
+        };
+      };
     };
   };
 
   networking.firewall = {
-    allowedTCPPorts = [443 853];
-    allowedUDPPorts = [53];
-  };
-
-  environment.persistence."/nix/persist" = {
-    directories = [
-      {
-        directory = "/var/cache/knot-resolver";
-        user = "knot-resolver";
-        group = "knot-resolver";
-        mode = "0770";
-      }
-    ];
+    allowedTCPPorts = [53 443 853];
+    allowedUDPPorts = [53 853];
   };
 }
