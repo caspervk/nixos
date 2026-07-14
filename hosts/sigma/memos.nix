d39681666b15771ecc22e079cb2584e1e749e724
Casper V. Kristensen <casper@vkristensen.dk>
2026-07-14 02:16:37 +0200

    memos: use nixos service rather than oci container

--- hosts/sigma/memos.nix
+++ hosts/sigma/memos.nix
@@ -1,27 +1,40 @@
-{...}: {
-  # https://www.usememos.com/docs/install/self-hosting
+{pkgs, ...}: {
+  # Open-source, self-hosted note-taking tool built for quick capture.
+  # Markdown-native, lightweight, and fully yours.
+  # https://usememos.com/
+  services.memos = {
+    enable = true;
+    # Use newer version to allow disabling link previews
+    # https://github.com/usememos/memos/commit/e3e4ae10512f514f71729779b5096d0d591c8cf4
+    # TODO: remove override
+    package = pkgs.memos.overrideAttrs (prevAttrs: {
+      version = "0.29.1-0038295bbc772b38425b6c7f9ca814e4d1e44260";
+      src = pkgs.fetchFromGitHub {
+        owner = "usememos";
+        repo = "memos";
+        rev = "0038295bbc772b38425b6c7f9ca814e4d1e44260";
+        hash = "sha256-/9WfV3tvt3nzQBAmJFp6YnRrXexw2eIjCggXVUOp+Vo=";
+      };
+      vendorHash = "sha256-nyUBXPC8nt+7s2jFHohF0PWBGky24ZSXWtSI4XVf2kU=";
+      memos-web = prevAttrs.memos-web.overrideAttrs (_: {
+        pnpmDeps = prevAttrs.memos-web.pnpmDeps.overrideAttrs (_: {
+          outputHash = "sha256-TVB1WkRdqzfZnEe4z75UVfj9t7nO0UdVoWejul9qiIw=";
+        });
+      });
+      # Upstream added a unit test (TestUserWebhookSigningSecretLifecycle) that
+      # resolves example.com; the build sandbox has no network.
+      doCheck = false;
+    });
+  };
 
-  virtualisation.oci-containers.containers = {
-    memos = {
-      # Don't upgrade past v0.24, see:
-      # https://github.com/mudkipme/MoeMemosAndroid/issues/264
-      # https://github.com/usememos/memos/issues/4896
-      image = "docker.io/neosmemo/memos:0.24";
-      labels = {
-        "io.containers.autoupdate" = "registry";
-      };
-      environment = {
-        # https://github.com/usememos/memos/issues/2433#issuecomment-1797316081
-        MEMOS_METRIC = "false";
-      };
-      ports = [
-        # TODO: for some *very* weird reason, exposing the port does not work
-        # if we use the same port on the host and inside the container. Why??
-        "127.0.0.1:5231:5230"
-      ];
-      volumes = [
-        "memos:/var/opt/memos"
-      ];
-    };
+  environment.persistence."/nix/persist" = {
+    directories = [
+      {
+        directory = "/var/lib/memos";
+        user = "memos";
+        group = "memos";
+        mode = "0750";
+      }
+    ];
   };
 }
